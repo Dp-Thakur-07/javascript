3579f3240185930cccf0b84f04a0109018e61394
durgaprasad parmar <152007241+Dp-Thakur-07@users.noreply.github.com>
2025-01-18 14:02:10 +0000

    Conversion of operation

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,29 @@
+let score = undefined
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+//"33"=> 33
+//"33abc"=>NaN
+//true = 1 , false = 0
+
+
+let isLoggedIn = "dp"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+//1=>true , 0=>flase
+//""=>false
+//"dp" = >true
+
+let somenumber = 33
+let stringName = String(somenumber)
+
+console.log(stringName)
+console.log(typeof stringName)
