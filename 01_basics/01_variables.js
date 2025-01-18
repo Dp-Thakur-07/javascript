91710d2fcf6fab70803f8c3053384d9c78e6ea75
durgaprasad parmar <152007241+Dp-Thakur-07@users.noreply.github.com>
2025-01-18 13:24:33 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 123421
+let accountEmail = "dpthakur@07"
+var accountpassword = "123"
+accountcity = "indore"
+
+/*
+prefer to not use var 
+because of the issue of block scope and functional scope
+*/
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountpassword,accountcity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"  //treat all js code as new version
+
+//alert(3+3) we are using nodejs , not browser
+
+console.log(3+
+    3         //code readability should be high
+) 
+
+//datatypes 
+
+let name = "DURGAPRASAD"
+let age = 23
+let isLoggedIn = false
+
+//number => 2 to power 52 
+//bigInt 
+//string =>""
+//boolean=> true/false
+//null => standlone value 
+//undefined=>
+//symbol => unique
+
+//object 
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);    // object
+
